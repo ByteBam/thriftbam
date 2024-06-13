765c8ed60cb7a89e1c6d63bd6ce4b64f515f6eb2
KingYen. <1696918097@qq.com>
2024-06-13 18:56:40 +0800

    docs: update swagger file

--- biz/server/http.go
+++ biz/server/http.go
@@ -1,11 +1,13 @@
 package server
 
 import (
+	"context"
 	"fmt"
 	"github.com/ByteBam/thirftbam/biz/handler"
 	_ "github.com/ByteBam/thirftbam/docs"
 	"github.com/ByteBam/thirftbam/pkg/utils/log"
 	"github.com/ByteBam/thirftbam/pkg/utils/server/http"
+	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/cloudwego/hertz/pkg/app/server"
 	"github.com/hertz-contrib/swagger"
 	"github.com/spf13/viper"
@@ -23,6 +25,9 @@ func NewHTTPServer(
 	)
 
 	url := fmt.Sprintf("http://%s/swagger/doc.json", conf.GetString("app.http.addr"))
+	s.Hertz.Any("/ping", func(c context.Context, ctx *app.RequestContext) {
+		ctx.JSON(200, "pong")
+	})
 	s.Hertz.GET("/swagger/*any", swagger.WrapHandler(swaggerFiles.Handler, swagger.URL(url)))
 	v1 := s.Hertz.Group("/api/v1")
 	v1.POST("/analyze", analyzeHandler.Analyze)
